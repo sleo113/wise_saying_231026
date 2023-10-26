f9512e581944ba882fc207d24fdbc65a7accb28c
JeUk Park <qkrwpdnr4531@naver.com>
2023-10-26 16:43:25 +0900

    make exit cmd

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,24 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     void run(){
+        int count = 0;
 
+        System.out.println("== 명언 앱 ==");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.printf("명령 : ");
+
+            String cmd = scanner.nextLine();
+            System.out.printf("입력하신 명령 : %s", cmd);
+
+            if(cmd.equals("종료")){
+                break;
+            }
+        }
     }
 }
